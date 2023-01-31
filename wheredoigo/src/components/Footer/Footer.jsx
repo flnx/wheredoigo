0bc755fda5a4924f348eb9d1209b81f055b4faa9
flnx <32278279+flnx@users.noreply.github.com>
2023-01-31 16:41:19 +0200

    Styling the footer and making it responsive

--- wheredoigo/src/components/Footer/Footer.jsx
+++ wheredoigo/src/components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ export const Footer = () => {
             <div className="container">
                 <div className={styles.wrapper}>
                     <section className={styles.intro}>
-                        <h3> Where Do I Go?</h3>
-                        <p>
+                        <h3 className={styles.title}> Where Do I Go?</h3>
+                        <p className={styles.link}>
                             We always strive to make our customers happy by
                             providing the best choices possible!
                         </p>
@@ -18,17 +18,17 @@ export const Footer = () => {
                         </div>
                     </section>
                     <section className={styles.about}>
-                        <h3>About</h3>
-                        <p>About us</p>
-                        <p>FAQ</p>
+                        <h3 className={styles.title}>About</h3>
+                        <p className={styles.link}>About us</p>
+                        <p className={styles.link}>FAQ</p>
                     </section>
                     <section className={styles.company}>
-                        <h3>Company</h3>
-                        <p>Why "Where Do I Go?"</p>
-                        <p>Our Team</p>
+                        <h3 className={styles.title}>Company</h3>
+                        <p className={styles.link}>Why "Where Do I Go?"</p>
+                        <p className={styles.link}>Our Team</p>
                     </section>
                     <section className={styles.newsletter}>
-                        <h3>Stay in touch with our weekly newsletter</h3>
+                        <h3 className={styles.title}>Stay in touch with our weekly newsletter</h3>
                         <input type="search" name="" id="" />
                     </section>
                 </div>
