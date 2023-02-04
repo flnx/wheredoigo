c7cf8425d41fb1cd6c31783ae50bc26ff259f87f
flnx <32278279+flnx@users.noreply.github.com>
2023-02-04 21:46:09 +0200

    Adding User Image to Side nav and fixing global box shadows

--- wheredoigo/src/components/SideNav/SideNavigation.jsx
+++ wheredoigo/src/components/SideNav/SideNavigation.jsx
@@ -4,6 +4,12 @@ import { NavLink } from 'react-router-dom';
 export const SideNavigation = () => {
     return (
         <div className={styles.sideNav}>
+            <header className={styles.userInfo}>
+                <img src="https://randomuser.me/api/portraits/women/28.jpg" alt="user image" />
+                <h2>Jessica Alba</h2>
+                <p>JessicaA@yahoo.com</p>
+            </header>
+
             <nav className={styles.navbar}>
                 <NavLink to="#" className={styles.navLink}>
                     Favorites
