3a8358982653722592ae0f40a9d070aa857e88fc
flnx <32278279+flnx@users.noreply.github.com>
2023-02-04 21:56:39 +0200

    Switching back to the old global font

--- wheredoigo/src/components/SideNav/SideNavigation.jsx
+++ wheredoigo/src/components/SideNav/SideNavigation.jsx
@@ -1,6 +1,8 @@
 import styles from './SideNavigation.module.css';
 import { NavLink } from 'react-router-dom';
 
+// TO DO: Add Icons
+
 export const SideNavigation = () => {
     return (
         <div className={styles.sideNav}>
