8705269187c4337db13ceb4aefc749741d1b5951
flnx <32278279+flnx@users.noreply.github.com>
2023-03-16 23:30:05 +0200

    Server init

---
+++ client/src/context/DiscoverContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+
+const DiscoverContext = createContext();
+
+export const DiscoverContextProvider = (props) => {
+    
+
+
+    return (
+        <DiscoverContext.Provider value={{}}>
+            {props.children}
+        </DiscoverContext.Provider>
+    )
+}
