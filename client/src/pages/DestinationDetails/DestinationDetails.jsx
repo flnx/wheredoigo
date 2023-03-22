b26201e611715e1df115d1221621ec4904ec4189
flnx <32278279+flnx@users.noreply.github.com>
2023-03-22 09:25:04 +0200

    Adding 1 section for all types and removing the old ones

--- client/src/pages/DestinationDetails/DestinationDetails.jsx
+++ client/src/pages/DestinationDetails/DestinationDetails.jsx
@@ -3,36 +3,60 @@ import { useDestination } from '../../hooks/queries/useDestination';
 
 // components
 import { DestinationHeader } from './components/Header/Header';
-import { EatSection } from './components/EatSection/EatSection';
-import { ExploreSection } from './components/ExploreSection/ExploreSection';
-import { PartySection } from './components/PartySection/PartySection';
+import { SectionSlider } from './components/ExploreSection/SectionSlider';
 import { TipsPopUp } from '../../components/TipsPopUp/TipsPopUp';
 
 import styles from './DestinationDetails.module.css';
 
 export const DestinationDetails = () => {
     const { destinationId } = useParams();
-    const destination = useDestination(destinationId);
+    const { isLoading, error, data } = useDestination(destinationId);
 
-    if (destination.isLoading) {
+    if (isLoading) {
         return <h1>Loading...</h1>;
     }
 
-    if (destination.error) {
+    if (error) {
         return <h1>An Error Has Occured</h1>;
     }
 
-    console.log(destination.data);
-    return
+    const explorePlaces = data.places.filter(
+        (x) => x.type.toLowerCase() == 'explore'
+    );
+    const partyPlaces = data.places.filter(
+        (x) => x.type.toLowerCase() == 'party'
+    );
+    const eatingPlaces = data.places.filter(
+        (x) => x.type.toLowerCase() == 'eat'
+    );
 
     return (
         <div className="container">
             <div className={styles.wrapper}>
-                <DestinationHeader destination={destination.data} />
-                <TipsPopUp details={destination.data.details} />
-                {/* <ExploreSection places={places.data} /> */}
-                {/* <EatSection places={places.data} /> */}
-                {/* <PartySection places={places.data} />  */}
+                <DestinationHeader destination={data} />
+                <TipsPopUp details={data.details} />
+                
+                {explorePlaces.length > 0 && (
+                    <SectionSlider
+                        places={explorePlaces}
+                        type={'Explore'}
+                        description={'lorem ipsum 10 and lorem ipsum 25'}
+                    />
+                )}
+                {partyPlaces.length > 0 && (
+                    <SectionSlider
+                        places={partyPlaces}
+                        type={'Party'}
+                        description={'lorem ipsum 10 and lorem ipsum 25'}
+                    />
+                )}
+                {eatingPlaces.length > 0 && (
+                    <SectionSlider
+                        places={eatingPlaces}
+                        type={'Eat'}
+                        description={'lorem ipsum 10 and lorem ipsum 25'}
+                    />
+                )}
             </div>
         </div>
     );
