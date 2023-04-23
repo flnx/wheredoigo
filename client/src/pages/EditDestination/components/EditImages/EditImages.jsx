480092a4e08f3c28a825abceca3ea5999cdbcebf
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-04-23 10:28:07 +0300

    Separating NewImagesUploader into another file

--- client/src/pages/EditDestination/components/EditImages/EditImages.jsx
+++ client/src/pages/EditDestination/components/EditImages/EditImages.jsx
@@ -1,16 +1,10 @@
-import { memo, useReducer, useState } from 'react';
+import { memo, useState } from 'react';
 import { useDeleteDestinationImage } from '../../../../hooks/queries/useDeleteDestinationImage';
-import { useAddDestinationNewImages } from '../../../../hooks/queries/useAddDestinationNewImages';
-import { imagesReducer, initialState } from '../../../../utils/imagesReducer';
-import { createImagesFormData } from '../../../../utils/formData';
 
 // Components
 import { ImagesPreviewer } from '../../../../components/ImagesPreviewer/ImagesPreviewer';
 import { ConfirmModal } from '../../../../components/ConfirmModal/ConfirmModal';
-import { UploadImagesPreview } from '../../../../components/UploadImagesPreview/UploadImagesPreview';
-import { SuccessButton } from '../../../../components/Buttons/Success-Button/SuccessButton';
-
-import styles from './EditImages.module.css';
+import { NewImagesUploader } from './NewImagesUploader';
 
 const EditImages = ({ imagesData, destinationId }) => {
     const [deleteImage, error, isLoading] = useDeleteDestinationImage(destinationId);
@@ -76,57 +70,6 @@ const EditImages = ({ imagesData, destinationId }) => {
                 </ConfirmModal>
             )}
         </>
-    );
-};
-
-const NewImagesUploader = ({ currentImagesHandler, destinationId }) => {
-    const [uploadImages, error, isLoading] = useAddDestinationNewImages(destinationId);
-    const [newImagesState, dispatch] = useReducer(imagesReducer, initialState);
-
-    const dispatchHandler = (action) => {
-        dispatch(action);
-    };
-
-    const handleNewImagesSubmit = async (e) => {
-        e.preventDefault();
-
-        if (!hasNewlyUploadImages || isLoading) return;
-
-        const formData = await createImagesFormData(newImagesState);
-
-        uploadImages(formData, {
-            onSuccess: (result) => {
-                dispatch({ type: 'reset' });
-                currentImagesHandler(result.imageUrls);
-            },
-            onError: (e) => {
-                dispatch({ type: 'reset' });
-            },
-        });
-    };
-
-    const hasNewlyUploadImages = newImagesState.imageUrls.length > 0;
-
-    return (
-        <div className={styles['newly-uploaded-images']}>
-            {error && (
-                <span className={styles.uploadError}>
-                    Sorry, there was an error during the upload process. Please try again
-                    later.
-                </span>
-            )}
-
-            <UploadImagesPreview
-                images={newImagesState.imageUrls}
-                dispatchHandler={dispatchHandler}
-            />
-
-            {hasNewlyUploadImages && (
-                <SuccessButton onClickHandler={handleNewImagesSubmit} isLoading={isLoading}>
-                    Add
-                </SuccessButton>
-            )}
-        </div>
     );
 };
 
