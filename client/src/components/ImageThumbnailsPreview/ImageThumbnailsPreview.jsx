8072741fe29f0f0cec05e49caad1d7abe19e9351
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-05-01 04:39:08 +0300

    Changing Dashboard styling

--- client/src/components/ImageThumbnailsPreview/ImageThumbnailsPreview.jsx
+++ client/src/components/ImageThumbnailsPreview/ImageThumbnailsPreview.jsx
@@ -4,15 +4,16 @@ import styles from './ImageThumbnailsPreview.module.css';
 
 export const ImageThumbnailsPreview = ({ images = [], handleDeleteImage }) => {
     return (
-        <div className={styles.imagesWrapper}>
+        <div className={`${styles.imagesWrapper}`}>
             {images.map((img, i) => (
                 <div
                     key={i}
                     className={styles.imgContainer}
                     onClick={() => handleDeleteImage(i)}
                 >
+                    <div className={styles.overlayText}>tap to remove</div>
                     <img src={extractUrl(img)} alt={`image preview ${i}`} />
-                    <X size={40} weight="thin" className={styles.remove} />
+                    <X size={50} weight="thin" className={styles.remove} />
                 </div>
             ))}
         </div>
