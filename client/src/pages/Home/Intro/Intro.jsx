c3eef67456089509706cc8ffcbadcc663b4a2074
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-05-02 10:20:03 +0300

    Update Intro.jsx

--- client/src/pages/Home/Intro/Intro.jsx
+++ client/src/pages/Home/Intro/Intro.jsx
@@ -1,23 +1,25 @@
-import styles from './Intro.module.css';
 import introImage from '../../../assets/pictures/homepage/intro.png';
-
-import { PrimaryButton } from '../../../components/Buttons/Primary-Btn/PrimaryButton';
 import { Container } from '../../../components/Containers/Container/Container';
+import { ButtonLinkPrimary } from '../../../components/Buttons/Primary-Btn/LinkButtonPrimary';
 
+
+import styles from './Intro.module.css';
 export const Intro = () => {
     return (
         <section className={styles.introSection}>
             <Container>
                 <div className={styles.wrapper}>
                     <div className={styles.imageContainer}>
-                        <img src={introImage} alt="person with a bagpack " />
+                        <img src={introImage} alt="person with a big bagpack" />
                     </div>
                     <div className={styles.intro}>
                         <h2 className={styles.title}>Travel any corner of the world</h2>
                         <p className={styles.content}>
                             Take a vacation with your friends and get rid of the boredom!
                         </p>
-                        <PrimaryButton>Explore</PrimaryButton>
+                        <ButtonLinkPrimary to={`/discover`}>
+                            Discover (change it)
+                        </ButtonLinkPrimary>
                     </div>
                 </div>
             </Container>
