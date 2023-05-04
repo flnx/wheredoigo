313beb1c2919c2639d1800f550418dcf6b0a282a
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-05-04 15:23:54 +0300

    removing arrows on first/last image

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -8,14 +8,14 @@ import styles from './Gallery.module.css';
 
 export const Gallery = ({ images = [], closeGalleryHandler }) => {
     const [mainImage, setMainImage] = useState(images[0]);
+    const mainImgIndex = images.findIndex((i) => i._id == mainImage._id);
 
     if (!Array.isArray(images) || images.length == 0) {
         return;
     }
 
     const onLeftArrowClickHandler = () => {
-        const imgIndex = images.findIndex((i) => i._id == mainImage._id);
-        const previousImage = images[imgIndex - 1];
+        const previousImage = images[mainImgIndex - 1];
 
         if (previousImage) {
             setMainImage(previousImage);
@@ -23,8 +23,7 @@ export const Gallery = ({ images = [], closeGalleryHandler }) => {
     };
 
     const onRightArrowClickHandler = () => {
-        const imgIndex = images.findIndex((i) => i._id == mainImage._id);
-        const nextImage = images[imgIndex + 1];
+        const nextImage = images[mainImgIndex + 1];
 
         if (nextImage) {
             setMainImage(nextImage);
@@ -37,6 +36,8 @@ export const Gallery = ({ images = [], closeGalleryHandler }) => {
     }, []);
 
     const isActive = styles.isActive;
+    const isFirstImage = mainImgIndex == 0;
+    const isLastImage = mainImgIndex == images.length - 1;
 
     return (
         <section>
@@ -47,13 +48,23 @@ export const Gallery = ({ images = [], closeGalleryHandler }) => {
                 </div>
                 <div className={styles.imagesContainer}>
                     <div className={styles.mainImgContainer}>
-                        <div className={styles.leftArrow}>
-                            <CaretLeft size={32} onClick={onLeftArrowClickHandler} />
-                        </div>
+                        {!isFirstImage && (
+                            <div
+                                className={styles.leftArrow}
+                                onClick={onLeftArrowClickHandler}
+                            >
+                                <CaretLeft size={32} />
+                            </div>
+                        )}
                         <img src={mainImage.imageUrl} alt="img" className={styles.mainImg} />
-                        <div className={styles.rightArrow}>
-                            <CaretRight size={32} onClick={onRightArrowClickHandler} />
-                        </div>
+                        {!isLastImage && (
+                            <div
+                                className={styles.rightArrow}
+                                onClick={onRightArrowClickHandler}
+                            >
+                                <CaretRight size={32} />
+                            </div>
+                        )}
                     </div>
                     <div className={styles.secondaryImgContainer}>
                         {images.map((x) => (
