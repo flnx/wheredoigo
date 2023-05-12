2c74ba1a1af7a64f45bf368c31b43e81a24ebcc5
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-05-12 17:17:41 +0300

    Adding responsive styles to nav categories

--- client/src/components/CategoriesNav/CategoriesNav.jsx
+++ client/src/components/CategoriesNav/CategoriesNav.jsx
@@ -5,23 +5,29 @@ import { Adventure, Beach, Culture, Islands, Mountain, Snow } from '../../utils/
 export const CategoriesNav = () => {
     return (
         <div className={styles.categories}>
-            <div className={styles.icon}>
-                <Adventure />
+            <div>
+                <Adventure className={styles.icon} />
+                <span>Adventure</span>
             </div>
-            <div className={styles.icon}>
-                <Beach />
+            <div>
+                <Beach className={styles.icon} />
+                <span>Beach</span>
             </div>
-            <div className={styles.icon}>
-                <Culture />
+            <div>
+                <Culture className={styles.icon} />
+                <span>Culture</span>
             </div>
-            <div className={styles.icon}>
-                <Islands />
+            <div>
+                <Islands className={styles.icon} />
+                <span>Islands</span>
             </div>
-            <div className={styles.icon}>
-                <Mountain />
+            <div>
+                <Mountain className={styles.icon} />
+                <span>Mountains</span>
             </div>
-            <div className={styles.icon}>
-                <Snow />
+            <div>
+                <Snow className={styles.icon} />
+                <span>Snow</span>
             </div>
         </div>
     );
