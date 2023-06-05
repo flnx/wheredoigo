29703f1f919dd5d4bb93fedad937c41e290a189d
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-06-05 13:34:31 +0300

    Categories Nav full tests added

--- client/src/components/CategoriesNav/__tests__/CategoriesNav.test.jsx
+++ client/src/components/CategoriesNav/__tests__/CategoriesNav.test.jsx
@@ -8,36 +8,77 @@ const render = (Component) => {
     return customRender(<MemoryRouter>{Component}</MemoryRouter>);
 };
 
-describe('AddToFavorites', () => {
-    it('Renders the Adventure Icon with text and if it passes the correct value', async () => {
-        const onClick = vi.fn();
+describe('CategoriesNav', () => {
+    let onClick;
 
+    beforeEach(() => {
+        onClick = vi.fn();
         render(<CategoriesNav onCategoryClickHandler={onClick} />);
+    });
 
+    it('Renders the Adventure Icon with text and it passes the coorect value to the handler ', async () => {
         const adventureIcon = screen.getByTestId('adventure-icon');
         const iconText = screen.getByText(/adventure/i);
 
         await userEvent.click(adventureIcon);
 
-        expect(iconText).toBeInTheDocument();
         expect(adventureIcon).toBeInTheDocument();
+        expect(iconText).toBeInTheDocument();
         expect(onClick).toHaveBeenCalledWith('Adventure');
     });
 
-    // it('Renders the Beach Icon with text', () => {
-    //     const onClick = vi.fn();
-    //     render(<CategoriesNav onCategoryClickHandler={onClick} />);
+    it('Renders the Beach Icon with text and it passes the coorect value to the handler ', async () => {
+        const beachIcon = screen.getByTestId('beach-icon');
+        const iconText = screen.getByText(/beach/i);
 
-    //     const adventureIcon = screen.getByTestId('beach-icon');
-    //     const iconText = screen.getByText(/beach/i);
+        await userEvent.click(beachIcon);
 
-    //     expect(iconText).toBeInTheDocument();
-    //     expect(adventureIcon).toBeInTheDocument();
-    // });
+        expect(beachIcon).toBeInTheDocument();
+        expect(iconText).toBeInTheDocument();
+        expect(onClick).toHaveBeenCalledWith('Beach');
+    });
+
+    it('Renders the Culture Icon with text and it passes the coorect value to the handler ', async () => {
+        const cultureIcon = screen.getByTestId('culture-icon');
+        const iconText = screen.getByText(/beach/i);
+
+        await userEvent.click(cultureIcon);
+
+        expect(cultureIcon).toBeInTheDocument();
+        expect(iconText).toBeInTheDocument();
+        expect(onClick).toHaveBeenCalledWith('Cultural');
+    });
+
+    it('Renders the Islands Icon with text and it passes the coorect value to the handler ', async () => {
+        const islandsIcon = screen.getByTestId('islands-icon');
+        const iconText = screen.getByText(/islands/i);
+
+        await userEvent.click(islandsIcon);
+
+        expect(islandsIcon).toBeInTheDocument();
+        expect(iconText).toBeInTheDocument();
+        expect(onClick).toHaveBeenCalledWith('Islands');
+    });
+
+    it('Renders the Islands Icon with text and it passes the coorect value to the handler ', async () => {
+        const mountainsIcon = screen.getByTestId('mountain-icon');
+        const iconText = screen.getByText(/mountains/i);
+
+        await userEvent.click(mountainsIcon);
+
+        expect(mountainsIcon).toBeInTheDocument();
+        expect(iconText).toBeInTheDocument();
+        expect(onClick).toHaveBeenCalledWith('Mountains');
+    });
+
+    it('Renders the Islands Icon with text and it passes the coorect value to the handler ', async () => {
+        const snowIcon = screen.getByTestId('snow-icon');
+        const iconText = screen.getByText(/snow/i);
+
+        await userEvent.click(snowIcon);
+
+        expect(snowIcon).toBeInTheDocument();
+        expect(iconText).toBeInTheDocument();
+        expect(onClick).toHaveBeenCalledWith('Snow');
+    });
 });
-
-// Beach
-// Cultural
-// Islands
-// Mountains
-// Snow
