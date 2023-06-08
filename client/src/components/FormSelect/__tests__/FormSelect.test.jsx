0af2c48eaf5e6e1d2709d522df44961bbf5fbadd
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-06-09 00:12:27 +0300

    Improving the test

--- client/src/components/FormSelect/__tests__/FormSelect.test.jsx
+++ client/src/components/FormSelect/__tests__/FormSelect.test.jsx
@@ -34,20 +34,24 @@ describe('FormSelect Tests', () => {
     });
 
     it('Should select the correct option', async () => {
-        render(<FormSelect {...props} />);
+        const { rerender } = render(<FormSelect {...props} />);
 
         // This Component relies complitely on a parent compponent state
         // This is why I can't simulate a real rerender while using userEvent
         // So I just check if the handler is being called
-        // I could use toHaveBeenCalledWith but I pass the whole "e" to the parent so its not possible
-        // [] I could reform my code to be cleaner be I'm maybe in the near future...
+        // I could use toHaveBeenCalledWith to check the prop name but I pass the whole "e" to the parent so its not possible
+        // [] I could reform my code to be cleaner and easier to test but maybe in the future...
 
-        const dropdown = screen.getByRole('combobox');
+        let dropdown = screen.getByRole('combobox');
         expect(dropdown).toHaveValue(''); // Initial selected value
 
         const optionToSelect = props.options[2]; // Varna
 
         await userEvent.selectOptions(dropdown, optionToSelect);
         expect(props.onChangeHandler).toHaveBeenCalledTimes(1);
+        props.value = optionToSelect;
+
+        rerender(<FormSelect {...props} />);
+        expect(screen.getByRole('option', { name: optionToSelect }).selected).toBe(true);
     });
 });
