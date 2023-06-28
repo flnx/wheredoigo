2faaddc70f5ee841461bea83af5b3453e7273a3a
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-06-28 20:47:28 +0300

    Removing aggregation and adding find() instead for top destinations

--- server/src/services/destinationServices/getMostLikedDestinations.js
+++ server/src/services/destinationServices/getMostLikedDestinations.js
@@ -2,51 +2,21 @@ const Destination = require('../../models/destinationSchema');
 const capitalizeEachWord = require('../../utils/capitalizeWords');
 
 async function getMostLikedDestinations() {
-    const destinations = await Destination.aggregate([
-        { $match: { $expr: { $gt: [{ $size: '$likes' }, 0] } } }, // at least 1 like match
-        {
-            $lookup: {
-                from: 'countries',
-                localField: 'country',
-                foreignField: '_id',
-                as: 'country',
-            },
-        },
-        { $unwind: '$country' },
-        {
-            $lookup: {
-                from: 'users',
-                localField: 'likes',
-                foreignField: '_id',
-                as: 'likesData',
-            },
-        },
-        {
-            $project: {
-                city: 1,
-                country: 1,
-                likesCount: { $size: '$likes' }, // count the total destination likes
-                likes: { $slice: ['$likesData', -3] }, // get the last 3 user likes
-                imageUrls: { $arrayElemAt: ['$imageUrls.imageUrl', 0] }, // extract the main img url
-            },
-        },
-        { $sort: { likesCount: -1 } }, // sort from high to low
-        { $limit: 12 },
-    ]).exec();
-
-    const destinations2 = await Destination.find({ likes: { $gt: [] } })
+    const destinations = await Destination.find({ likes: { $gt: [] } })
         .sort({ likesCount: -1 })
         .select({
             city: 1,
             country: 1,
             likesCount: 1,
-            imageUrls: { $arrayElemAt: ['$imageUrls.imageUrl', 0] },
-            likes: { $slice: -3 },
+            imageUrls: { $slice: 1 }, // get the main img (img on index 0)
+            likes: { $slice: -3 }, // slice the last 3 likes
         })
-        .limit(1)
+        .populate([
+            { path: 'country', select: 'name -_id' },
+            { path: 'likes', select: 'username avatarUrl -_id' },
+        ])
+        .limit(12)
         .exec();
-
-    console.log(destinations2);
 
     const updatedDestinations = destinations.map((destination) => {
         const { _id, city, country, imageUrls, likes, likesCount } = destination;
@@ -54,13 +24,10 @@ async function getMostLikedDestinations() {
         return {
             _id,
             likesCount,
-            imageUrls,
+            imageUrls: imageUrls[0].imageUrl,
             city: capitalizeEachWord(city),
             country: capitalizeEachWord(country.name),
-            lastUserLikes: likes.map((user) => ({
-                username: user.username,
-                avatarUrl: user.avatarUrl,
-            })),
+            lastUserLikes: likes,
         };
     });
 
