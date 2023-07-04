ac1b833cb40b70f3d57a9b1e389eed28b72783a8
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-07-04 07:06:37 +0300

    Refactor: Adding comments to addAIGeneratedCommentsToPlace

--- server/src/services/placeServices/addAIGeneratedCommentsToPlace.js
+++ server/src/services/placeServices/addAIGeneratedCommentsToPlace.js
@@ -12,6 +12,7 @@ const { attachIDsToComments } = require('../../utils/attachIDsToComments');
 async function addAIGeneratedCommentsToPlace(place, commenters) {
     const placeId = place._id.toString();
 
+    // Using virtuals
     const placeDataToComment = {
         name: place.capitalizedName,
         country: place.capitalizedCountry,
@@ -19,26 +20,31 @@ async function addAIGeneratedCommentsToPlace(place, commenters) {
         numOfCommenters: commenters.length,
     };
 
+    // Generate comments using AI model
     const comments = await commentsGeneratedByAI(placeDataToComment);
-
+    
+    // Attach IDs to the comments
     const updatedComments = attachIDsToComments({
         comments,
         commenters,
         placeId,
     });
 
+    // Add the updated comments to the place
     const { 
         updateResult, 
         commentRatingSum, 
         numOfComments 
     } = await addCommentsToPlace(updatedComments, placeId);
 
+    // Calculate the new average rating for the place
     const averageRating = calcAverageRating(
         place.rating,
         commentRatingSum,
         numOfComments
     );
 
+    // Return the result with updated fields
     return {
         ...updateResult,
         averageRating,
@@ -48,17 +54,24 @@ async function addAIGeneratedCommentsToPlace(place, commenters) {
 }
 
 async function addCommentsToPlace(comments, placeId) {
+    // Start a database session
     const session = await mongoose.startSession();
 
     try {
+        // Start a transaction for the session
         session.startTransaction();
 
+        // Insert the comments into the Comment collection (with the session)
         const addedComments = await Comment.insertMany(comments, { session });
 
+        // Extract comment IDs and owner IDs from added comments
         const commentIds = addedComments.map((comment) => comment._id);
         const ownerIds = addedComments.map((comment) => comment.ownerId);
+
+        // Calculate the sum of comment ratings
         const commentRatingSum = addedComments.reduce((sum, c) => sum + c.rating, 0);
 
+        // Prepare the filter and update objects for updating the Place collection
         const filter = {
             _id: placeId,
             commentedBy: { $not: { $in: ownerIds } },
@@ -75,14 +88,18 @@ async function addCommentsToPlace(comments, placeId) {
             },
         };
 
+        // Update the place document with the new comments and ratings
         const updateResult = await Place.updateOne(filter, update, { session });
 
+        // Throw an error if the update operation does not modify exactly one document
         if (updateResult.modifiedCount !== 1) {
             throw createValidationError(errorMessages.unavailable, 503);
         }
 
+        // Commit the transaction
         await session.commitTransaction();
-
+        
+        // Return the update result along with comment rating sum and number of comments
         return {
             updateResult,
             commentRatingSum,
@@ -90,9 +107,12 @@ async function addCommentsToPlace(comments, placeId) {
         };
     } catch (error) {
         console.log(`addCommentsToPlace - ${error.message}`);
+
+        // Abort the transaction and throw a validation error
         await session.abortTransaction();
         throw createValidationError(errorMessages.serverError, 500);
     } finally {
+        // End the session
         session.endSession();
     }
 }
