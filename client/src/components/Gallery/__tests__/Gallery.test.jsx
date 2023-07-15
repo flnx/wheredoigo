085869dfb2a234d6408d6f77f308eaca6cef314f
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-07-15 14:17:43 +0300

    Adding Gallery tests for the transformed secondary images

--- client/src/components/Gallery/__tests__/Gallery.test.jsx
+++ client/src/components/Gallery/__tests__/Gallery.test.jsx
@@ -1,26 +1,32 @@
 import { render, screen, userEvent, waitFor } from '../../../utils/test-utils';
 import { Gallery } from '../Gallery';
+import { applyCloudinaryTransformation } from '../../../utils/utils';
 
 const props = {
     images: [
         {
-            imageUrl: 'https://images.pexels.com/photos/2440021/pexels-photo-2440021.jpeg?auto=compress&cs=tinysrgb&w=600&h=750&dpr=1',
+            imageUrl:
+                'https://res.cloudinary.com/degidchop/image/upload/v1688464712/cld-sample-3.jpg',
             _id: 'id1',
         },
         {
-            imageUrl: 'https://images.pexels.com/photos/325807/pexels-photo-325807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            imageUrl:
+                'https://res.cloudinary.com/degidchop/image/upload/v1688464712/cld-sample-4.jpg',
             _id: 'id2',
         },
         {
-            imageUrl: 'https://images.pexels.com/photos/1324803/pexels-photo-1324803.jpeg?auto=compress&cs=tinysrgb&w=600&h=750&dpr=1',
+            imageUrl:
+                'https://res.cloudinary.com/degidchop/image/upload/v1688464712/cld-sample-2.jpg',
             _id: 'id3',
         },
         {
-            imageUrl: 'https://images.pexels.com/photos/1108701/pexels-photo-1108701.jpeg?auto=compress&cs=tinysrgb&w=600&h=750&dpr=1',
+            imageUrl:
+                'https://res.cloudinary.com/degidchop/image/upload/v1688464712/cld-sample.jpg',
             _id: 'id4',
         },
         {
-            imageUrl: 'https://images.pexels.com/photos/335966/pexels-photo-335966.jpeg?auto=compress&cs=tinysrgb&w=600&h=750&dpr=1',
+            imageUrl:
+                'https://res.cloudinary.com/degidchop/image/upload/v1688464712/cld-sample-5.jpg',
             _id: 'id5',
         },
     ],
@@ -30,6 +36,8 @@ const props = {
 
 describe('Gallery correctly renders the images and sets the main image', () => {
     beforeEach(() => {
+        // scrollIntoView doesnt exist outside the browser..
+        Element.prototype.scrollIntoView = vi.fn();
         props.closeGalleryHandler = vi.fn();
         render(<Gallery {...props} />);
     });
@@ -49,25 +57,31 @@ describe('Gallery correctly renders the images and sets the main image', () => {
         expect(mainImage).toHaveAttribute('src', url);
     });
 
-    it('Correctly renders the secondary images', () => {
+    it('Correctly renders the secondary cloudinary images (transformed)', () => {
         const images = screen.getAllByAltText(new RegExp(props.alt, 'i'));
 
         // Check if the secondary images are being shown with their valid urls
         // Cuts off the first main image with slice in order to get only the secondary images
         images.slice(1).forEach((image, i) => {
             const validImageUrl = props.images[i].imageUrl;
-            expect(image).toHaveAttribute('src', validImageUrl);
+            const transformedImage = applyCloudinaryTransformation(validImageUrl);
+
+            expect(image).toHaveAttribute('src', transformedImage);
         });
     });
 
-    it('When clicked on a secondary image, it sets it as a MAIN Image', async () => {
+    it.only('When clicked on a secondary image, it sets it as a MAIN Image', async () => {
         let mainImage;
         let expectedImgUrl;
 
         // Click on a image to set it as a "Main Image"
         const imageToSetAsMain = screen.getByAltText(`${props.alt} 2`);
         expectedImgUrl = props.images[1].imageUrl;
-        expect(imageToSetAsMain).toHaveAttribute('src', expectedImgUrl);
+
+        expect(imageToSetAsMain).toHaveAttribute(
+            'src',
+            applyCloudinaryTransformation(expectedImgUrl) // expects resized image
+        );
 
         // Clicks on one of the secondary images to set it as a "Main image";
         await userEvent.click(imageToSetAsMain);
@@ -77,7 +91,11 @@ describe('Gallery correctly renders the images and sets the main image', () => {
         // Click on another image to set it as a "Main Image"
         const anotherImageToSetAsMain = screen.getByAltText(`${props.alt} 3`);
         expectedImgUrl = props.images[2].imageUrl;
-        expect(anotherImageToSetAsMain).toHaveAttribute('src', expectedImgUrl);
+
+        expect(anotherImageToSetAsMain).toHaveAttribute(
+            'src',
+            applyCloudinaryTransformation(expectedImgUrl)
+        );
 
         // Clicks on one of the secondary images to set it as a "Main image";
         await userEvent.click(anotherImageToSetAsMain);
