a871bad40e69d2c06d0f5c651470e739e1333c6c
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-07-17 20:20:12 +0300

    removing div margin and adding it on header tags on edit place /desti

--- client/src/pages/EditDestination/components/Form/components/DetailsFormFields.jsx
+++ client/src/pages/EditDestination/components/Form/components/DetailsFormFields.jsx
@@ -8,20 +8,25 @@ export const DetailsFormFields = ({
     error,
     details,
 }) => {
-    return details.map((detail) => (
-        <div key={detail._id}>
-            <h3>{detail.name}</h3>
-            <MemoizedFormFieldEditor
-                fieldId={detail._id}
-                title={detail.name}
-                desc={detail.content}
-                onEditButtonClickHandler={onEditButtonClickHandler}
-                isEditable={isEditable[detail._id]}
-                sendEditedFieldClickHandler={sendEditedFieldClickHandler}
-                isLoading={isLoading}
-                error={error}
-                categoryId={detail._id}
-            />
-        </div>
-    ));
+    return (
+        <>
+            <h3>Destination Details</h3>
+
+            {details.map((detail) => (
+                <div key={detail._id}>
+                    <MemoizedFormFieldEditor
+                        fieldId={detail._id}
+                        title={detail.name}
+                        desc={detail.content}
+                        onEditButtonClickHandler={onEditButtonClickHandler}
+                        isEditable={isEditable[detail._id]}
+                        sendEditedFieldClickHandler={sendEditedFieldClickHandler}
+                        isLoading={isLoading}
+                        error={error}
+                        categoryId={detail._id}
+                    />
+                </div>
+            ))}
+        </>
+    );
 };
