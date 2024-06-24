b715ae7282ebd3569c683cfe21b891960ee70638
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2024-06-24 10:52:04 +0300

    Fixing Auth Buttons spacing

--- client/src/components/Navbar/components/NavLinksDesktop/NavLinksDesktop.jsx
+++ client/src/components/Navbar/components/NavLinksDesktop/NavLinksDesktop.jsx
@@ -13,13 +13,20 @@ export const NavLinksDesktop = () => {
     const { auth } = useContext(AuthContext);
     const { DISCOVER, DASHBOARD } = routeConstants;
 
+    const isAuthenticated = auth.accessToken;
+
     return (
         <nav>
-            <ul className={styles.navbar}>
+            <ul
+                className={styles.navbar}
+                style={{
+                    gap: isAuthenticated ? '3rem' : '1.25rem',
+                }}
+            >
                 <Link to={DISCOVER.route} className={styles.btn}>
                     {DISCOVER.name}
                 </Link>
-                {!auth.accessToken ? (
+                {!isAuthenticated ? (
                     <UnauthenticatedRoutesLinks />
                 ) : (
                     <Link to={DASHBOARD.route} className={styles.btn}>
