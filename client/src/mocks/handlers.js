e1bdd72f5f3208107123233d230aebb9fd161c2f
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2024-07-14 23:21:55 +0300

    fixing mock test endpoints

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -3,15 +3,15 @@ import { apiEndpoints } from '../constants/apiEndpoints';
 import { likeSuccess, newlyAddedImages } from './exampleMocks';
 
 export const handlers = [
-    rest.post(apiEndpoints.likeDestination('testId', 'like'), (req, res, ctx) => {
+    rest.post(apiEndpoints.destination.like('testId', 'like'), (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(likeSuccess));
     }),
 
-    rest.put(apiEndpoints.addDestinationImages('testId'), (req, res, ctx) => {
+    rest.put(apiEndpoints.destination.addImages('testId'), (req, res, ctx) => {
         return res(ctx.status(200), ctx.json(newlyAddedImages));
     }),
 
-    rest.put(apiEndpoints.deleteDestinationImage('testId'), (req, res, ctx) => {
+    rest.put(apiEndpoints.destination.deleteImage('testId'), (req, res, ctx) => {
         return res(ctx.status(200), ctx.json({ deleted: true }));
     }),
 ];
